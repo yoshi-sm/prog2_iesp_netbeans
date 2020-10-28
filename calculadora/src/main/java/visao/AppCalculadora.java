716c4a960d2a00f63136a0c8bc5dc8a58b459b9e
yoshi-sm <yoshismiranda@gmail.com>
2020-10-28 12:53:47 -0300

    commit 2

--- calculadora/src/main/java/visao/AppCalculadora.java
+++ calculadora/src/main/java/visao/AppCalculadora.java
@@ -11,7 +11,7 @@ import modelo.*;
  */
 
 
-public class NewJFrame extends javax.swing.JFrame {
+public class AppCalculadora extends javax.swing.JFrame {
     double num1, num2;
     String operador;
     Equacao eqn = new Equacao(num1,num2,operador);
@@ -47,7 +47,7 @@ public class NewJFrame extends javax.swing.JFrame {
     /**
      * Creates new form NewJFrame
      */
-    public NewJFrame() {
+    public AppCalculadora() {
         initComponents();
     }
 
@@ -492,20 +492,21 @@ public class NewJFrame extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AppCalculadora.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AppCalculadora.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AppCalculadora.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AppCalculadora.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new NewJFrame().setVisible(true);
+                new AppCalculadora().setVisible(true);
             }
         });
     }
